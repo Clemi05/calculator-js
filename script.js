febdce879d5c2bc00d731b0d88820ed1f37294bf
Clement Azalbert <clementazalbert04@gmail.com>
2022-07-06 01:04:06 +0200

    created Calculator class + constructor

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+}
+
 // Variables
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
